f2598171525955288a415d9a6177a1b6b1afee9b
Alexey Samofalov <samofalov91@gmail.com>
2020-10-04 21:30:59 +0400

    поправил автокомплит по городам

--- #src/js/script.js
+++ #src/js/script.js
@@ -12,7 +12,6 @@ new Vue({
         textSearch: '',
         result: [],
         show: false,
-        fruit: ['Apple', 'Banana', 'Orange', 'Mango', 'Pear', 'Peach', 'Grape', 'Tangerine', 'Pineapple'],
         count: []
     },
     watch: {
@@ -22,10 +21,9 @@ new Vue({
     },
     methods: {
         getCounter() {
-            let arr2 = []
-            fetch('https://gist.githubusercontent.com/gorborukov/0722a93c35dfba96337b/raw/435b297ac6d90d13a68935e1ec7a69a225969e58/russia')
-                .then(r => r.json())
-                .then(i => this.count.push(i))
+            axios.get('https://gist.githubusercontent.com/gorborukov/0722a93c35dfba96337b/raw/435b297ac6d90d13a68935e1ec7a69a225969e58/russia').then(response => {
+            this.count = response.data.map(i => i.city)
+            })
         },
         Change() {
             if (this.textSearch.length != 0) {
@@ -37,7 +35,7 @@ new Vue({
                 this.show = false
         },
         filterResults() {
-            this.result = this.count[0].filter(item => item.city.toLowerCase().indexOf(this.textSearch.toLowerCase()) > -1)
+            this.result = this.count.filter(item => item.toLowerCase().indexOf(this.textSearch.toLowerCase()) > -1)
         },
         setResults(result) {
             this.textSearch = result
